2fdc82bc76ee44cd7e62264e27eb158b083a9b61
Christian Duan <christianduan2002@gmail.com>
2024-07-30 18:44:28 -0700

    Added picture

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import NavBar from "@/components/navBar";
 import AboutMe from "@/components/AboutMe";
+import { Box, Typography } from "@mui/material";
+import Projects from "@/components/projects";
 
 export default function Home() {
   return (
@@ -10,7 +12,35 @@ export default function Home() {
       height: '100vh'
     }}>
       <NavBar/>
-      <AboutMe/>
+
+      <Box sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: '20px'
+      }}>
+        <AboutMe/>
+        <Box sx={{ flexShrink: 0, marginLeft: '20px' }}>
+          <img src="/me.jpg" alt="Me"
+            style={{
+              width: '200px',
+              borderRadius: '50%',
+            }}/>
+        </Box>
+      </Box>
+
+      <Box sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        padding: '20px'
+      }}>
+        <Typography variant="h4" style={{ color: 'white' }}>
+          Projects
+        </Typography>
+      </Box>
+
+      <Projects/>
+      
     </div>
   );
 }
